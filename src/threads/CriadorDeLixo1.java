78a158fc1fb608dacd78821ce33b0517315457ba
Eduardo Brito <157918380+Eduard0w@users.noreply.github.com>
2025-11-18 07:11:59 -0300

    Add files via upload

--- src/threads/CriadorDeLixo1.java
+++ src/threads/CriadorDeLixo1.java
@@ -2,34 +2,16 @@ package threads;
 
 public class CriadorDeLixo1 extends Thread{
 	int cont = 0;
+	String[] tiposLixo = {"plastico", "metal", "organico", "vidro", "papel"};
 	public void run() {
-		while(true) {
-			//Tentar usar uma thread para gerar valores aleatorios para escolher(aleatoriamente) qual tipo de lixo será gerado.
+		while(cont < 10) {
+			System.out.println("Criando um lixo " + tiposLixo[cont]);
+			try {
+				Thread.sleep(1000);
+			}catch(Exception e) {
+				e.getStackTrace();
+			}
+			cont++;
 		}
-//		while(cont < 10) {
-//			System.out.println("Criando um lixo do tipo 1");
-//			try {
-//				Thread.sleep(1000);
-//			}catch(Exception e) {
-//				e.getStackTrace();
-//			}
-//			cont++;
-//		}
-	}
-	
-	public static String tipo1() {
-		return "lixo plastico";
-	}
-	public static String tipo2() {
-		return "lixo metal";
-	}
-	public static String tipo3() {
-		return "lixo organico";
-	}
-	public static String tipo4() {
-		return "lixo vidro";
-	}
-	public static String tipo5() {
-		return "lixo papel";
 	}
 }
